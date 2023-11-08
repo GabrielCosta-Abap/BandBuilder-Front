05d8b3aca16436d7159e34c4d292c2b03ff8b9e7
f3lip3mp <lipe.nupe@gmail.com>
2023-11-08 00:11:28 -0300

    finaliza as telas de cadastro

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +15,19 @@ root.render(
   // </React.StrictMode>
 );
 
+
+function storeValues() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const born = document.getElementById("born").value;
+  const tel = document.getElementById("tel").value;
+  const password = document.getElementById("password").value;
+
+  localStorage.setItem("name", name);
+  localStorage.setItem("born", born);
+  localStorage.setItem("email", email);
+  localStorage.setItem("tel", tel);
+  localStorage.setItem("password", password);
+}
+
+export default storeValues;
