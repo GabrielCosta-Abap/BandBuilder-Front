0d20823aef003cc9c82ee060583970071d449251
f3lip3mp <lipe.nupe@gmail.com>
2023-11-08 00:20:54 -0300

    finaliza telas de cadastro

--- src/index.js
+++ src/index.js
@@ -14,20 +14,3 @@ root.render(
   //   <App />
   // </React.StrictMode>
 );
-
-
-function storeValues() {
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const born = document.getElementById("born").value;
-  const tel = document.getElementById("tel").value;
-  const password = document.getElementById("password").value;
-
-  localStorage.setItem("name", name);
-  localStorage.setItem("born", born);
-  localStorage.setItem("email", email);
-  localStorage.setItem("tel", tel);
-  localStorage.setItem("password", password);
-}
-
-export default storeValues;
